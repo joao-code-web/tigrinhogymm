02950ebebeb95057266ac038def9e381d1d4dd56
joao-code-web <joaogabriel9512@gmail.com>
2024-03-28 16:00:40 -0300

    xama

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import GetTransactions from "@/components/Hooks/GetTransactions"; // Corrigindo o caminho do hook useGetTransactions
