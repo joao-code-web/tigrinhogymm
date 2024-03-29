75a819040ca078e894d9bc2972c9466eb0d0a70d
joao-code-web <joaogabriel9512@gmail.com>
2024-03-29 09:29:30 -0300

    xama

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import "./page.css"
 
 interface Transaction {
   _id: string;
@@ -11,11 +12,31 @@ interface Transaction {
 export default function Home() {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [formData, setFormData] = useState<{ name: string; value: string }>({ name: '', value: '' });
+  const [allSomTransaction, setAllSumTransaction] = useState<number>(0);
+  const [positive, setPositive] = useState<number>(0);
+  const [negative, setNegative] = useState<number>(0);
 
   const fetchTransactions = async () => {
     try {
       const response = await axios.get<Transaction[]>('/api/transactions');
       setTransactions(response.data);
+
+      let sum = 0;
+      let positiveSum = 0;
+      let negativeSum = 0;
+
+      response.data.forEach(transaction => {
+        sum += transaction.value;
+        if (transaction.value > 0) {
+          positiveSum += transaction.value;
+        } else {
+          negativeSum += transaction.value;
+        }
+      });
+
+      setAllSumTransaction(sum);
+      setPositive(positiveSum);
+      setNegative(negativeSum);
     } catch (error) {
       console.error('Erro ao buscar transações:', error);
     }
@@ -50,27 +71,53 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <h1>Transações</h1>
+    <div className='displayAll'>
+      <h1>Me paga ivan</h1>
+
+      <div className="dashboard">
+        <div className="dashTotal">
+          <h2>Total: {allSomTransaction}</h2>
+        </div>
+        <div className="dashPositive">
+          <h2>Positivo: {positive}</h2>
+        </div>
+        <div className="dashNegative">
+          <h2>Negativo: {negative}</h2>
+        </div>
+      </div>
+
       <form onSubmit={handleSubmit}>
-        <label>
-          Nome:
-          <input type="text" name="name" value={formData.name} onChange={handleInputChange} />
-        </label>
-        <label>
-          Valor:
-          <input type="number" name="value" value={formData.value} onChange={handleInputChange} />
-        </label>
-        <button type="submit">Enviar Transação</button>
+        <div className="form-group">
+          <label htmlFor="name">Nome:</label>
+          <input type="text" id="name" name="name" value={formData.name} onChange={handleInputChange} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="value">Valor:</label>
+          <input type="number" id="value" name="value" value={formData.value} onChange={handleInputChange} />
+        </div>
+        <button type="submit" className='buttonAdd'>Enviar Transação</button>
       </form>
-      <ul>
-        {transactions.map((transaction) => (
-          <li key={transaction._id}>
-            {transaction.name} - R$ {transaction.value}{' '}
-            <button onClick={() => handleDelete(transaction._id)}>Excluir</button>
-          </li>
-        ))}
-      </ul>
+
+      <div className="tableContainer">
+        <table>
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>Valor</th>
+              <th>Ação</th>
+            </tr>
+          </thead>
+          <tbody>
+            {transactions.map((transaction, index) => (
+              <tr key={transaction._id}>
+                <td>{transaction.name}</td>
+                <td>{transaction.value}</td>
+                <td><button onClick={() => handleDelete(transaction._id)}>Deletar</button></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
